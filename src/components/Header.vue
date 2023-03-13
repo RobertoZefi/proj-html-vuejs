3ab8ebbe8d4b94927ceb5082800b9ad2eb6eba6e
RobertoZefi <zefi.roberto2001@gmail.com>
2023-03-13 18:00:14 +0100

    FEAT section 1 done

---
+++ src/components/Header.vue
@@ -0,0 +1,129 @@
+<script>
+    export default{
+        data(){
+            return{
+                headerOptions: [
+                    'Home',
+                    'Who We Are',
+                    'What We Do',
+                    'Where We Work',
+                    'Careers',
+                    'News'
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <header>
+        <nav class="container">
+            <img src="/images/logo_seo_w_1x.png" alt="">
+            <div class="nav-options">
+                <ul class="options">
+                    <li v-for="el in headerOptions">{{ el }}</li> 
+                </ul>
+                <a class="button" href="#">get in touch now</a>
+            </div>
+        </nav>
+
+        <div class="container hero">
+            <div class="hero-description">
+                <h1>SEM Campaigns Made Simple With Avada</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis amet velit error inventore officia iure.</p>
+                <div class="hero-links">
+                    <a class="hero-button" href="">buy avada now</a>
+                    <a class="hero-button" href="">contact us</a>
+                </div>
+            </div>
+            <img src="/images/best-sem-seo-services-slide-2.png" alt="">
+        </div>
+    </header>
+    
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables.scss';
+
+header{
+    background-image: url('/images/pattern_background.png');
+    background-color: #361E5C;
+    background-size: contain;
+    color: white;
+    height: 850px;
+}
+
+nav{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-bottom: 130px;
+    padding-top: 30px;
+}
+
+.nav-options{
+    display: flex;
+    align-items: center;
+    gap: 40px;
+}
+.options{
+    display: flex;
+    gap: 35px;
+}
+
+.button{
+    text-transform: uppercase;
+    background-color: #FFA837;
+    padding: 10px 18px;
+    border-radius: 999px;
+    font-weight: bold;
+    font-size: 13px;
+}
+
+.hero{
+    display: flex;
+    align-items: center;
+
+    img{
+        flex-basis: 70%;
+    }
+}
+.hero-description{
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+
+    h1{
+        font-size: 60px;
+    }
+
+    p{
+        font-size: 20px;
+        font-weight: 300;
+        width: 500px;
+        line-height: 35px;
+    }
+
+    .hero-links{
+        display: flex;
+        gap: 20px;
+    }
+}
+
+.hero-button{
+    text-transform: uppercase;
+    padding: 15px 50px;
+    border-radius: 999px;
+    font-weight: bold;
+    font-size: 13px;
+}
+
+.hero-button:first-child{
+    background-color: #FFA837;
+}
+
+.hero-button:last-child{
+    background-color: #556EED;
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,75 @@
+<script>
+    export default{
+        data(){
+            return{
+                
+            }
+        }
+    }
+</script>
+
+<template>
+    <main>
+        <section class="section-1">
+            <div class="container-sm">
+                <h1 class="title">Meet The Bew Agency SEO Template From The Avada Team</h1>
+                <p class="subtitle">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor mollitia ipsa eveniet excepturi!</p>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="card">
+                        <i class="fa-solid fa-road"></i>
+                        <h2>Establish Goal</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat pariatur doloribus laborum fuga labore iusto sequi dicta ea id quae?</p>
+                    </div>
+                    <div class="card">
+                        <i class="fa-solid fa-video"></i>
+                        <h2>Work With A Team</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat pariatur doloribus laborum fuga labore iusto sequi dicta ea id quae?</p>
+                    </div>
+                    <div class="card">
+                        <i class="fa-solid fa-droplet"></i>
+                        <h2>Get Result</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat pariatur doloribus laborum fuga labore iusto sequi dicta ea id quae?</p>
+                    </div>
+                </div>
+                <img src="/images/agency-seo-desk-front-1200x570.jpg" alt="">
+            </div>
+        </section>
+    </main>
+</template>
+
+<style scoped lang="scss">
+@use '../style/partials/variables.scss';
+
+.section-1{
+    background-color: #F7F8FA;
+
+    .row{
+        gap: 25px;
+    }
+    .card{
+        text-align: center;
+        background-color: white;
+        padding: 40px 25px;
+        display: flex;
+        flex-direction: column;
+        gap: 25px;
+
+        p{
+            line-height: 30px;
+        }
+
+        i{
+            font-size: 30px;
+            color: #FFA837;
+        }
+    }
+
+    img{
+        display: block;
+        margin: 0 auto;
+    }
+}
+
+</style>
