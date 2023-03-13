fd644b1f65b42d01479a5c6e3b857753fc6080ac
RobertoZefi <zefi.roberto2001@gmail.com>
2023-03-13 21:24:14 +0100

    FEAT section-3 done

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -36,6 +36,59 @@
                 <img src="/images/agency-seo-desk-front-1200x570.jpg" alt="">
             </div>
         </section>
+
+        <section class="section-2">
+            <div class="container-sm">
+                <h1 class="title">See Our Top Notch Services</h1>
+                <p class="subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero provident sint impedit harum.</p>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="card">
+                        <i class="fa-brands fa-google"></i>
+                        <h2>Google SEO</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam asperiores non eius incidunt nam pariatur adipisci placeat explicabo libero perspiciatis? </p>
+                        <p class="info">Read more ></p>
+                    </div>
+                    <div class="card">
+                        <i class="fa-solid fa-gear"></i>
+                        <h2>Brand Strategy</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam asperiores non eius incidunt nam pariatur adipisci placeat explicabo libero perspiciatis? </p>
+                        <p class="info">Read more ></p>
+                    </div>
+                    <div class="card">
+                        <i class="fa-solid fa-building"></i>
+                        <h2>Local SEO</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam asperiores non eius incidunt nam pariatur adipisci placeat explicabo libero perspiciatis? </p>
+                        <p class="info">Read more ></p>
+                    </div>
+                    <div class="card">
+                        <i class="fa-solid fa-chart-column"></i>
+                        <h2>SEO Analysis</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam asperiores non eius incidunt nam pariatur adipisci placeat explicabo libero perspiciatis? </p>
+                        <p class="info">Read more ></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section-3">
+            <div class="container-sm">
+                <h1 class="title">Our Work</h1>
+                <p class="subtitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas obcaecati eum explicabo hic.</p>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <img src="/images/case-study-gallery-3-1-400x300.jpg" alt="">
+                    <img src="/images/case-study-gallery-2-400x300.jpg" alt="">
+                    <img src="/images/case-study-gallery-1-1-400x300.jpg" alt="">
+                    <img src="/images/case-study-gallery-4-1-400x300.jpg" alt="">
+                    <img src="/images/case-study-gallery-5-1-400x300.jpg" alt="">
+                    <img src="/images/case-study-gallery-6-1-400x300.jpg" alt="">
+                </div>
+                <a class="button" href="#">view our work</a>
+            </div>
+        </section>
     </main>
 </template>
 
@@ -64,6 +117,7 @@
             font-size: 30px;
             color: #FFA837;
         }
+
     }
 
     img{
@@ -72,4 +126,72 @@
     }
 }
 
+
+.section-2{
+    background-color: #051642;
+    background-image: url('/images/pattern_background.png');
+    background-size: contain;
+    color: white;
+    padding-bottom: 130px;
+    .subtitle{
+        color: white;
+    }
+    .row{
+        gap: 25px;
+    }
+    .card{
+        text-align: center;
+        background-color: #071B50;
+        padding: 40px 25px;
+        display: flex;
+        flex-direction: column;
+        gap: 25px;
+
+        p{
+            line-height: 30px;
+        }
+
+        i{
+            font-size: 30px;
+            color: #FFA837;
+        }
+
+        .info{
+            color: #FFA837;
+        }
+    }
+
+    img{
+        display: block;
+        margin: 0 auto;
+    }
+}
+
+.section-3{
+
+    padding-bottom: 80px;
+    .container{
+        text-align: center;
+    }
+    .row{
+        flex-wrap: wrap;
+        gap: 20px;
+        justify-content: center;
+    }
+    .img{
+        flex-basis: calc(100% / 3);
+    }
+    
+    .button{
+        text-transform: uppercase;
+        background-color: #FFA837;
+        padding: 10px 25px;
+        border-radius: 999px;
+        font-weight: bold;
+        font-size: 13px;
+        color: white;
+        display: inline-block;
+        margin-top: 50px;
+    }
+}
 </style>
